918cd097f1410d0c09e385f91ad4bcc03cae7dd9
ashim-sharma-console <ashimsharma49@gmail.com>
2025-07-05 23:09:05 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,24 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <h1>GenZ</h1>
+
+      <nav>
+        <a href="/">Posts</a> |{" "}
+        <a href="/users">Users</a> |{" "}
+        <a href="/notifications">Notifications</a>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
+        <Route path="/editPost/:postId" element={<EditPostForm />} />
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/users/:userId" element={<UserPosts />} />
+        <Route path="/notifications" element={<NotificationsList />} />
+      </Routes>
+
+      <AddPostForm />
     </div>
   )
 }
